52b1847103b3be95e6d6c05ba7cf5a477b8efd12
zekyou <1339056492@qq.com>
2020-02-12 17:21:15 +0900

    Site updated: 2020-02-12 17:21:15

---
+++ js/FunnyTitle.js
@@ -0,0 +1,19 @@
+<!--浏览器搞笑标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ有东西！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ看错了！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+  });
+
+
