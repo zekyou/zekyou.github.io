ea6a2212dd468bb9f93b5e84eeae80d1cf05bf26
zekyou <1339056492@qq.com>
2020-02-12 22:48:25 +0900

    Site updated: 2020-02-12 22:48:24

---
+++ js/toc.js
@@ -0,0 +1,31 @@
+(function() {
+	'use strict';
+
+	function light(config){
+
+	}
+	light.prototype.init = function() {
+		this.tocFixed();
+		
+	};
+	//make toc stay in the visible area
+	light.prototype.tocFixed = function(){
+		var HEADER_OFFSET = 0;
+		var $toc = $('#post-toc');
+		if ($toc.length){
+			var minScrollTop = $toc.offset().top;
+			$(window).scroll(function(){
+				var scrollTop = $(window).scrollTop();
+				if (scrollTop <minScrollTop){
+					$toc.css({'position':'absolute','top':'0px','right':'-400px'});
+				} else {
+				//	$toc.css({'position':'fixed','top':'20px','right':'50px'});
+					$toc.css({'top':scrollTop+'px'});
+				}
+			});
+		}
+	};
+
+	var light = new light();
+	light.init();
+})(window);
